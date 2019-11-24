01f453e7635bcec890815963e6b477e3555b8495
tashaffi samin <tashaffiyeasar@gmail.com>
2019-11-24 18:36:28 +0600

    function to extract entities from dataset

--- NamedEntityMatching.py
+++ NamedEntityMatching.py
@@ -1,12 +1,76 @@
-import spacy
+# Convert .tsv file to dataturks json format. 
+import json
+import logging
+import sys
 
-def extract_objects():
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(u"Tashaffi is looking at buying Mona Lisa painting for $1 billion dollars.")
-    objects = []
+def tsv_to_json_format(input_path,output_path,unknown_label):
+    try:
+        f=open(input_path,'r') # input file
+        fp=open(output_path, 'w') # output file
 
-    for ent in doc.ents:
-        print(ent.text, ent.start_char, ent.end_char, ent.label_)
-        objects.append(ent.label_)
+        data_dict={}
+        annotations =[]
+        label_dict={}
+        s=''
+        start=0
 
-    return objects
+        for line in f:
+            if line[0:len(line)-1]!='.\tO':
+                word,entity=line.split('\t')
+                s+=word+" "
+                entity=entity[:len(entity)-1]
+                if entity!=unknown_label:
+                    if len(entity) != 1:
+                        d={}
+                        d['text']=word
+                        d['start']=start
+                        d['end']=start+len(word)-1  
+                        try:
+                            label_dict[entity].append(d)
+                        except:
+                            label_dict[entity]=[]
+                            label_dict[entity].append(d) 
+
+                    start+=len(word)+1
+            else:
+                data_dict['content']=s
+                s=''
+                label_list=[]
+                for ents in list(label_dict.keys()):
+                    for i in range(len(label_dict[ents])):
+                        if(label_dict[ents][i]['text']!=''):
+                            l=[ents,label_dict[ents][i]]
+                            for j in range(i+1,len(label_dict[ents])): 
+                                if(label_dict[ents][i]['text']==label_dict[ents][j]['text']):  
+                                    di={}
+                                    di['start']=label_dict[ents][j]['start']
+                                    di['end']=label_dict[ents][j]['end']
+                                    di['text']=label_dict[ents][i]['text']
+                                    l.append(di)
+                                    label_dict[ents][j]['text']=''
+                            label_list.append(l)
+
+
+                for entities in label_list:
+                    label={}
+                    label['label']=[entities[0]]
+                    label['points']=entities[1:]
+                    annotations.append(label)
+
+                data_dict['annotation']=annotations
+                annotations=[]
+                json.dump(data_dict, fp)
+                fp.write('\n')
+                data_dict={}
+                start=0
+                label_dict={}
+
+            
+
+
+    except Exception as e:
+        logging.exception("Unable to process file" + "\n" + "error = " + str(e))
+        return None
+
+
+tsv_to_json_format("/Users/tashaffi/Documents/NER_data.tsv",'/Users/tashaffi/Documents/ner_corpus_260.json','abc')
